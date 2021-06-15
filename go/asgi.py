c612ea6dc73d9452607a0b3bebed2396dd912f95
Thanh Dang <iniad@LAPTOP-SM0LCRV3.localdomain>
2021-06-15 20:26:17 +0900

    Fix asgi.py

--- go/asgi.py
+++ go/asgi.py
@@ -1,7 +1,3 @@
-from django.core.asgi import get_asgi_application
-
-django_asgi_app = get_asgi_application()
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -9,7 +5,7 @@ import api.routing
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'go.settings')
-
+# django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
