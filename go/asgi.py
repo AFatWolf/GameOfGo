f18a31f7132be5dc997735862b7f4923010038cb
Thanh Dang <iniad@LAPTOP-SM0LCRV3.localdomain>
2021-06-15 21:03:54 +0900

    Fix not registered app error

--- go/asgi.py
+++ go/asgi.py
@@ -1,3 +1,6 @@
+import django
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -5,7 +8,9 @@ import api.routing
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'go.settings')
+
 # django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
